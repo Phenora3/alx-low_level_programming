4b7697ddfcdf14606ca7dc44f7b232efb0cc6636
Phenora3 <phenoraboateng1@gmail.com>
2022-12-15 15:49:07 -0800

    Task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - test fuction that prints if interger is positive or negative
+ * Return: 0
+ */
+int main (void)
+{
+	int i;
+	{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+	}
+}
