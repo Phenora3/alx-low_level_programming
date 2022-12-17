d7b8a1230cdb8f345d0b7eafd07c5db5fad8fa87
Phenora3 <phenoraboateng1@gmail.com>
2022-12-16 16:47:08 -0800

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,24 +7,17 @@
  */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
 	int x, y;
 
 	for (x = 0; x < n; x++)
 	{
-	for (y = 0; y < x; y++)
-	{
-	if ( y == x)
-	_putchar('\\');
-	else if (y < x)
-	_putchar(' ');
+		for (y = 0; y < x; y++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (x < (n - 1))
+			_putchar('\n');
 	}
 	_putchar('\n');
-	}
-	}
 }
